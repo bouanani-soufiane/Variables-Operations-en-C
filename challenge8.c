caecf2fa8281ca751aa028f2def32d1faa861b18
unknown <soufian.bouanani2013@gmail.com>
2023-09-13 17:11:25 +0100

    updated

--- challenge8.c
+++ challenge8.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int nombre;
+    int nbr, x1, x2, x3, n1, n2;
 
-    printf("Entrez un nombre entier : ");
-    scanf("%d", &nombre);
 
-    printf("La valeur en octal est : %o\n", nombre);
-
-    printf("La valeur en hexadécimal est : %X\n", nombre);
+    printf("saisir le nombre : ");
+    scanf("%d", &nbr);
+    n1 = nbr / 8;
+    x1 = nbr %8 ;
+    n2 = n1 / 8;
+    x2 = n1 % 8;
+    x3 = n2 % 8;
+    printf("%d %d %d", x3,x2,x1);
 
     return 0;
 }
